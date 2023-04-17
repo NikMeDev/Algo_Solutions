6055959ef0bd9d0979ee354c548db7d7e30e5723
Artiukh Nikita <devnikme@gmail.com>
2023-04-18 00:23:18 +0300

    4A

---
+++ 2_semester/4_contest/4A.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+
+static constexpr int64_t kInfinity = std::numeric_limits<int64_t>::max();
+
+struct Edge {
+  int64_t to_vertex;
+  int64_t capacity;
+  int64_t flow;
+
+  int64_t CurrentCapacity() const { return capacity - flow; }
+
+  Edge(int64_t to_vertex, int64_t capacity)
+      : to_vertex(to_vertex), capacity(capacity), flow(0) {}
+};
+
+void AddEdge(std::vector<std::vector<int64_t>>& graph, std::vector<Edge>& edges,
+             int64_t from, int64_t to, int64_t capacity) {
+  --from;
+  --to;
+  edges.emplace_back(to, capacity);
+  graph[from].emplace_back(edges.size() - 1);
+  edges.emplace_back(from, 0);
+  graph[to].emplace_back(edges.size() - 1);
+}
+
+struct State {
+  size_t phase;
+  int64_t current;
+  int64_t end;
+  int64_t min;
+};
+
+int64_t FindIncreasingPath(std::vector<std::vector<int64_t>>& graph,
+                           std::vector<Edge>& edges, std::vector<size_t>& used,
+                           State state) {
+  if (state.end == state.current) {
+    return state.min;
+  }
+  used[state.current] = state.phase;
+  for (int64_t edge_id : graph[state.current]) {
+    if (edges[edge_id].CurrentCapacity() > 0 &&
+        used[edges[edge_id].to_vertex] != state.phase) {
+      int64_t delta = FindIncreasingPath(
+          graph, edges, used,
+          {state.phase, edges[edge_id].to_vertex, state.end,
+           std::min(state.min, edges[edge_id].CurrentCapacity())});
+      if (delta > 0) {
+        edges[edge_id].flow += delta;
+        edges[edge_id ^ 1].flow -= delta;
+        return delta;
+      }
+    }
+  }
+  return 0;
+}
+
+int64_t MaxFlow(std::vector<std::vector<int64_t>>& graph,
+                std::vector<Edge>& edges, int64_t start, int64_t end) {
+  std::vector<size_t> used(graph.size(), 0);
+  size_t phase = 1;
+  while (FindIncreasingPath(graph, edges, used,
+                            {phase, start, end, kInfinity}) != 0) {
+    ++phase;
+  }
+
+  int64_t result = 0;
+  for (int64_t edge_id : graph[start]) {
+    result += edges[edge_id].flow;
+  }
+  return result;
+}
+
+int main() {
+  std::vector<Edge> edges;
+  int64_t vertex_count;
+  int64_t edge_count;
+  std::cin >> vertex_count >> edge_count;
+  std::vector<std::vector<int64_t>> graph(vertex_count, std::vector<int64_t>());
+  for (int64_t i = 0; i < edge_count; ++i) {
+    int64_t from;
+    int64_t to;
+    int64_t capacity;
+    std::cin >> from >> to >> capacity;
+    AddEdge(graph, edges, from, to, capacity);
+  }
+  std::cout << MaxFlow(graph, edges, 0, vertex_count - 1);
+  return 0;
+}
+#include <iostream>
+#include <limits>
+#include <vector>
+
+static constexpr int64_t kInfinity = std::numeric_limits<int64_t>::max();
+
+struct Edge {
+  int64_t to_vertex;
+  int64_t capacity;
+  int64_t flow;
+
+  int64_t CurrentCapacity() const { return capacity - flow; }
+
+  Edge(int64_t to_vertex, int64_t capacity)
+      : to_vertex(to_vertex), capacity(capacity), flow(0) {}
+};
+
+void AddEdge(std::vector<std::vector<int64_t>>& graph, std::vector<Edge>& edges,
+             int64_t from, int64_t to, int64_t capacity) {
+  --from;
+  --to;
+  edges.emplace_back(to, capacity);
+  graph[from].emplace_back(edges.size() - 1);
+  edges.emplace_back(from, 0);
+  graph[to].emplace_back(edges.size() - 1);
+}
+
+struct State {
+  size_t phase;
+  int64_t current;
+  int64_t end;
+  int64_t min;
+};
+
+int64_t FindIncreasingPath(std::vector<std::vector<int64_t>>& graph,
+                           std::vector<Edge>& edges, std::vector<size_t>& used,
+                           State state) {
+  if (state.end == state.current) {
+    return state.min;
+  }
+  used[state.current] = state.phase;
+  for (int64_t edge_id : graph[state.current]) {
+    if (edges[edge_id].CurrentCapacity() > 0 &&
+        used[edges[edge_id].to_vertex] != state.phase) {
+      int64_t delta = FindIncreasingPath(
+          graph, edges, used,
+          {state.phase, edges[edge_id].to_vertex, state.end,
+           std::min(state.min, edges[edge_id].CurrentCapacity())});
+      if (delta > 0) {
+        edges[edge_id].flow += delta;
+        edges[edge_id ^ 1].flow -= delta;
+        return delta;
+      }
+    }
+  }
+  return 0;
+}
+
+int64_t MaxFlow(std::vector<std::vector<int64_t>>& graph,
+                std::vector<Edge>& edges, int64_t start, int64_t end) {
+  std::vector<size_t> used(graph.size(), 0);
+  size_t phase = 1;
+  while (FindIncreasingPath(graph, edges, used,
+                            {phase, start, end, kInfinity}) != 0) {
+    ++phase;
+  }
+
+  int64_t result = 0;
+  for (int64_t edge_id : graph[start]) {
+    result += edges[edge_id].flow;
+  }
+  return result;
+}
+
+int main() {
+  std::vector<Edge> edges;
+  int64_t vertex_count;
+  int64_t edge_count;
+  std::cin >> vertex_count >> edge_count;
+  std::vector<std::vector<int64_t>> graph(vertex_count, std::vector<int64_t>());
+  for (int64_t i = 0; i < edge_count; ++i) {
+    int64_t from;
+    int64_t to;
+    int64_t capacity;
+    std::cin >> from >> to >> capacity;
+    AddEdge(graph, edges, from, to, capacity);
+  }
+  std::cout << MaxFlow(graph, edges, 0, vertex_count - 1);
+  return 0;
+}
